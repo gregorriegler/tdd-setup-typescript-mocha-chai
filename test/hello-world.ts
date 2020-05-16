fb02f49644d358600d1bd02c414b4d2003882348
Gregor Riegler <gregor.riegler@ebcont.com>
2020-05-16 18:04:08 +0200

    reverse one word

--- test/hello-world.ts
+++ test/hello-world.ts
@@ -2,11 +2,55 @@ import {expect} from "chai";
 
 describe('oddWords', function () {
     it('world', function () {
-
-        function oddWords(text: string) {
-            return "a"
+        type Input = {
+            read: () => string
         }
 
-        expect(oddWords("a")).to.equal("a")
+        type Output = {
+            write: (char: string) => void
+            get: () => string;
+        }
+
+        function createInput(text: string): Input {
+            let index = 0
+            return {
+                read: (): string => {
+                    return text.charAt(index++)
+                }
+            }
+        }
+
+        function createOutput(): Output {
+            let text: string = ""
+            return {
+                write: (char: string) => {
+                    text += char
+                },
+                get: (): string => {
+                    return text
+                }
+            }
+        }
+
+        function recursive(input: Input, output: Output) {
+            let char = input.read()
+
+            if (char != '.') {
+                recursive(input, output);
+                output.write(char)
+            }
+        }
+
+        function oddWords(text: string) {
+            let input = createInput(text)
+
+            let output = createOutput()
+
+            recursive(input, output);
+            output.write(".")
+            return output.get()
+        }
+
+        expect(oddWords("ab.")).to.equal("ba.")
     });
 });
